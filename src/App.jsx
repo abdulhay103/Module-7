c06d02489cc433deda8dee046f5ff7071d20f2c1
abdulhay103 <mdabdulhay95@gmail.comm>
2023-07-17 11:21:00 +0600

    Footer and header done

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,21 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Header from "./components/header/Header";
+import Header from "./components/headers/Header";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
   return (
-    // <Router>
-    //     <Header />
-    //     <Routes>
-    //       <Route exact path="/" Component={Home} />
-    //       <Route exact path="/about" Component={About} />
-    //     </Routes>
-    // </Router>
-    <div className="w-full min-h-screen bg-orange-100">
-      <Header />
-    </div>
+    <Router>
+      <div className="w-full min-h-screen bg-orange-100">
+        <Header />
+        <Routes>
+          <Route exact path="/" Component={Home} />
+          <Route exact path="/about" Component={About} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 };
 
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className=" fixed bottom-0 py-3 bg-white w-full text-center">
+      <p className=" text-slate-600 font-light">
+        React Learning with USTAD Module@7
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
